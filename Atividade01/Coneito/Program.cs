64bb6083f33d93b4bc363e21cbe1bc86b82a8248
abner <abner>
2025-04-28 09:48:16 -0300

    ajuste

---
+++ Atividade01/Coneito/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Informe a nota: ");
+        char nota = char.Parse(Console.ReadLine());
+
+        switch (nota)
+        {
+            case 'A':
+                Console.WriteLine("Excelente");
+                break;
+
+            case 'B':
+                Console.WriteLine("Bom");
+                break;
+
+            case 'C':
+                Console.WriteLine("Regular");
+                break;
+
+            case 'D':
+                Console.WriteLine("Ruim");
+                break;
+
+            case 'E':
+                Console.WriteLine("Péssimo");
+                break;
+
+            default:
+                Console.WriteLine("Inesixtente");
+                break;
+        }
+    }
+}
